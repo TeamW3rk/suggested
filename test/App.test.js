f43b68a0c618137132544da23f7ed67ec2783e39
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-03-05 16:00:50 -0800

    completed enzyme testing

--- test/App.test.js
+++ test/App.test.js
@@ -18,6 +18,23 @@ describe('RestaurantList component', () => {
     const wrapper = shallow(<RestaurantList list={fakeList} scroll={fakeFunc1} getId={fakeFunc2}/>);
     expect(wrapper).toMatchSnapshot();
   });
+
+  test('renders header content', () => {
+    const fakeList = [];
+    const fakeFunc1 = jest.fn();
+    const fakeFunc2 = jest.fn();
+    const wrapper = shallow(<RestaurantList list={fakeList} scroll={fakeFunc1} getId={fakeFunc2}/>);
+    expect(wrapper.find('#header').text()).toEqual('Need to book at 7:00 PM? Other great restaurants with availability.');
+  });
+
+  test('renders buttons', () => {
+    const fakeList = [];
+    const fakeFunc1 = jest.fn();
+    const fakeFunc2 = jest.fn();
+    const wrapper = shallow(<RestaurantList list={fakeList} scroll={fakeFunc1} getId={fakeFunc2}/>);
+    expect(wrapper.find('#left-scroll').length).toEqual(1);
+    expect(wrapper.find('#right-scroll').length).toEqual(1);
+  });
 })
 
 describe('SuggestedRestaurant component', () => {
@@ -26,6 +43,27 @@ describe('SuggestedRestaurant component', () => {
     const fakeFunc1 = jest.fn();
     const wrapper = shallow(<SuggestedRestaurant restaurant={fakeRestaurant} getId={fakeFunc1} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('render 1 suggestedRestaurant', () => {
+    const fakeRestaurant = {};
+    const fakeFunc1 = jest.fn();
+    const wrapper = shallow(<SuggestedRestaurant restaurant={fakeRestaurant} getId={fakeFunc1} />);
+    expect(wrapper.find('#suggested-restaurant').length).toEqual(1);
+  });
+
+  test('render 5 stars', () => {
+    const fakeRestaurant = {};
+    const fakeFunc1 = jest.fn();
+    const wrapper = shallow(<SuggestedRestaurant restaurant={fakeRestaurant} getId={fakeFunc1} />);
+    expect(wrapper.find('.fa-star').length).toEqual(5);
+  });
+
+  test('render 1 restaurant image', () => {
+    const fakeRestaurant = {};
+    const fakeFunc1 = jest.fn();
+    const wrapper = shallow(<SuggestedRestaurant restaurant={fakeRestaurant} getId={fakeFunc1} />);
+    expect(wrapper.find('#suggested-restaurant-image').length).toEqual(1);
   });
 })
 
