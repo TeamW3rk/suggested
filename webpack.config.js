3e71c26231cb3b8efc8a33467b4dc739a99aef9f
Bryan Duong <bryandduong@gmail.com>
2018-03-26 17:18:07 -0700

    refactor for ssr with client and server file

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,7 @@ var CompressionPlugin = require('compression-webpack-plugin');
 var BUILD_DIR = path.resolve(__dirname, 'public');
 var APP_DIR = path.resolve(__dirname, 'public');
 
-var config = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
+const common = {
   module : {
     loaders : [
       {
@@ -22,6 +17,14 @@ var config = {
   },
   externals: {
     "jquery": "jQuery"
+  },
+};
+
+const client = {
+  entry: APP_DIR + '/client.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
   },
   plugins: [
     new webpack.DefinePlugin({ // <-- key to reducing React's size
@@ -42,4 +45,25 @@ var config = {
   ]
 };
 
-module.exports = config;
+const server = {
+  entry: APP_DIR + '/server.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle-server.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({ // <-- key to reducing React's size
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(), //dedupe similar code 
+    new webpack.optimize.UglifyJsPlugin(), //minify everything
+    new webpack.optimize.AggressiveMergingPlugin(),//Merge chunks 
+  ]
+};
+
+module.exports = [
+  Object.assign({}, common, client),
+  Object.assign({}, common, server)
+];
